e95d8f8bbc8228dfceea2c55f4ae96b3bde86ef4
thergh <lukasz.ciswicki@gmail.com>
2025-03-24 22:30:41 +0100

    RestApplication

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/lcc/rest/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/lcc/rest/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.lcc.rest.rest;
 
 import com.lcc.rest.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,19 +11,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
+
 @RestController
 @RequestMapping("/api")
 public class StudentRestController{
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents;
+
+    @PostConstruct
+    public void loadData(){
+        theStudents = new ArrayList<>();
         theStudents.add(new Student("Poornima", "Patel"));
         theStudents.add(new Student("Mario", "Rossi"));
         theStudents.add(new Student("Mary", "Smith"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents(){
         return theStudents;
     }
 
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        return theStudents.get(studentId);
+    }
 
 }
